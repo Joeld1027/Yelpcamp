0ae40adde9571d97fa66514d652dab69565b5606
Joel D Infante <joeld1027@gmail.com>
2022-06-28 22:44:57 -0500

    improved looks using bootstrap updated all views

--- seed/index.js
+++ seed/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 20;
 		const camp = new Campground({
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: "https://picsum.photos/800/500",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, eum placeat? Delectus eos nemo aut ratione veritatis beatae molestias, quas minus, harum repellendus quis! Quos est quia voluptates fugiat excepturi?",
+			price: price,
 		});
 		await camp.save();
 	}
